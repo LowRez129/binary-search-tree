f0f73ae0c7607b05718214bda802c6001b927a02
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-10-21 15:08:35 +1300

    Edit *

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,25 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Binary Search Tree',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  optimization: {
+      runtimeChunk: 'single',
+  }
 
 };
